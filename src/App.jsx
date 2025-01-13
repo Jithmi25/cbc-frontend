b99c02afa5fd70df8f636d30db6277a0501685a1
Jithmi <wickjitha@gmail.com>
2025-01-13 18:31:13 +0530

    Admin page 🤴

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import LoginPage from './pages/loginPage';
-import HomePage from './pages/start';
-import SignupPage from './pages/signUpPage';
-
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/loginPage";
+import SignupPage from "./pages/signUpPage";
+import AdminHomePage from "./pages/admin/adminHomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-       <BrowserRouter>
-        <Routes> 
-          <Route path="/Login" element={<LoginPage />} />
-          <Route path="*" element={<HomePage/>} /> 
-          <Route path="/SignUp" element={<SignupPage/>}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signUp" element={<SignupPage />} />
+          <Route path="/adminHome/*" element={<AdminHomePage />} />
         </Routes>
-      </BrowserRouter> 
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
-
---
+++ src/pages/admin/adminHomePage.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { FaTachometerAlt, FaBox, FaShoppingCart, FaUsers } from "react-icons/fa";
+
+export default function AdminHomePage() {
+  return (
+    <div className="bg-pink-50 w-full h-screen flex">
+      {/* Sidebar */}
+      <div className="w-[20%] h-screen flex bg-pink-500 flex-col items-center py-4">
+        <Link
+          className="flex flex-row items-center mb-4 text-white hover:text-black"
+          to="/adminHome/dashboard"
+        >
+          <FaTachometerAlt className="mr-2" />
+          Dashboard
+        </Link>
+        <Link
+          className="flex flex-row items-center mb-4 text-white hover:text-black"
+          to="/adminHome/products"
+        >
+          <FaBox className="mr-2" />
+          Products
+        </Link>
+        <Link
+          className="flex flex-row items-center mb-4 text-white hover:text-black"
+          to="/adminHome/orders"
+        >
+          <FaShoppingCart className="mr-2" />
+          Orders
+        </Link>
+        <Link
+          className="flex flex-row items-center mb-4 text-white hover:text-black"
+          to="/adminHome/customers"
+        >
+          <FaUsers className="mr-2" />
+          Customers
+        </Link>
+      </div>
+
+      {/* Main Content */}
+      <div className="bg-pink-200 w-[80%] h-screen p-4">
+        <Routes>
+          <Route path="dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="products" element={<h1>Products</h1>} />
+          <Route path="orders" element={<h1>Orders</h1>} />
+          <Route path="customers" element={<h1>Customers</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
